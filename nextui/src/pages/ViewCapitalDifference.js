a3e96b66d6d8b804bf82d185f7644f495072f144
Tim <44584257+T303@users.noreply.github.com>
2022-05-29 11:59:26 -0500

    Updated USD conversion

--- nextui/src/pages/ViewCapitalDifference.js
+++ nextui/src/pages/ViewCapitalDifference.js
@@ -1,10 +1,11 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import { TransactionsContext } from "../TransactionsContext";
 function ViewCapitalDifference() {
     const {transactions} = useContext(TransactionsContext);
     let wallet = transactions.walletTransactions;
     let exchange = transactions.exchangeTransactions;
-    let miningTransactions = [];
+    const [miningTransactions, setMiningTransactions] = useState([]);
+    //let miningTransactions = [];
     let exchangeTransactions = [];
 
     function generateReport(){
@@ -14,18 +15,27 @@ function ViewCapitalDifference() {
                 method: 'GET',
                 redirect: 'follow'
             };
-            let valueInUSD;
             fetch(`http://localhost:5000/price?epoch=${element.timeStamp}`, requestOptions)
             .then(response => response.text())
-            .then(result => valueInUSD = result)
+            .then(result => {
+                console.log(result);
+                let walletTransaction = {
+                    value: result,
+                    date: element.timeStamp
+                }
+                console.log("mining transaction during populating: " + JSON.stringify(walletTransaction, null, 2));
+                let tempMiningTransactions = miningTransactions;
+                tempMiningTransactions.push(walletTransaction)
+                setMiningTransactions(tempMiningTransactions);
+                //miningTransactions.push(walletTransaction);
+            })
+            .then(console.log("mining transactions after populating: " + JSON.stringify(miningTransactions, null, 2)))
             .catch(error => console.log('error', error));
-                
-            let walletTransaction = {
-                value: valueInUSD,
-                date: element.timeStamp
-            }
-            miningTransactions.push(walletTransaction);
         });
+
+        console.log("When mining transactions has been populated it is: " + JSON.stringify(miningTransactions, null, 2))
+        
+
 
         exchange.forEach(element => {
             if(element["Transaction Type"] === "Sell"){
@@ -38,13 +48,26 @@ function ViewCapitalDifference() {
             
         });
 
-        console.log(JSON.stringify(miningTransactions, null, 2))
+        
+    }
+
+    function addElement(){
+        let walletTransaction = {
+            value: 1,
+            date: 2
+        }
+
+        let tempMiningTransactions = miningTransactions;
+        tempMiningTransactions.push(walletTransaction)
+        setMiningTransactions(tempMiningTransactions);
+        //miningTransactions.push(walletTransaction);
     }
     
 
     return ( 
         <div>
             <button className="btn btn-primary" onClick={generateReport}>Generate Capital Gain/Loss report</button>
+            <button className="btn btn-primary" onClick={addElement}>Add sample Transaction to miningTransactions</button>
             <p>{JSON.stringify(miningTransactions, null, 2)}</p>
             <p>{JSON.stringify(exchangeTransactions, null, 2)}</p>
         </div>
