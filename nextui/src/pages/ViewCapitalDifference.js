9566929cf56632d83f5090af5f7648b482d10e1f
Tim <44584257+T303@users.noreply.github.com>
2022-05-29 23:24:30 -0500

    successfully generated wallet income
    Found epoch and income pairs for Ethereum mining income using historical data from the end of day ethereum price on the day I recieved a payments from my mining pool

--- nextui/src/pages/ViewCapitalDifference.js
+++ nextui/src/pages/ViewCapitalDifference.js
@@ -1,5 +1,69 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { TransactionsContext } from "../TransactionsContext";
+import moment from 'moment';
+function ViewCapitalDifference() {
+    const {transactions} = useContext(TransactionsContext);
+    let wallet = transactions.walletTransactions;
+    let exchange = transactions.exchangeTransactions;
+    const [miningTransactions, setMiningTransactions] = useState([]);
+    const [exchangeTransactions, setExchangeTransactions] = useState([]);
+    const newmoment = Math.ceil(1610654014 / 86400) * 86400
+    console.log(newmoment);
+
+    function epochToEndOfDay(epoch){
+        return Math.ceil(epoch / 86400) * 86400
+    }
+
+    function toEther(wei){
+        return  wei / Math.pow(10, 18);
+    }
+
+    useEffect(() => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=1610654014&to=1644395655", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            let pricesAsArray = result.prices;
+            let associativeArray = {};
+            pricesAsArray.forEach(element => {
+                let epoch = element[0];
+                let priceAtEpoch = element[1]
+                associativeArray[epoch] = priceAtEpoch; 
+                
+            });
+            console.log(associativeArray);
+            
+
+            const walletTransactions = transactions.walletTransactions;
+            // * associativeArray[(epochToEndOfDay(element.timeStamp) * 1000)]
+            const incomeTransactionsFromWallet = walletTransactions.filter(element => element.to === "0x750ca59270bdf16507ff977037a49a8cfb6f98b9" && element.value != 0).map(element => ({epoch: element.timeStamp, value: toEther(element.value) * associativeArray[(epochToEndOfDay(element.timeStamp) * 1000)]}));
+            setMiningTransactions(incomeTransactionsFromWallet);
+
+            const exchangeTransactions = transactions.exchangeTransactions;
+            const sellingTransactionsFromExchange = exchangeTransactions.filter(element => element["Transaction Type"] === "Sell").map(element => ({Date: element["Date & time"], Proceeds: element["Proceeds (excl. fees paid) (USD)"]}));
+            setExchangeTransactions(sellingTransactionsFromExchange);
+        })
+        .catch(error => console.log('error', error));
+
+        
+    }, []);
+
+    return (
+        <div>
+            <pre>{JSON.stringify(miningTransactions, null, 2)}</pre>
+            <p>{JSON.stringify(exchangeTransactions, null, 2)}</p>
+        </div>
+    );
+}
+
+export default ViewCapitalDifference;
+
+
+/*
 function ViewCapitalDifference() {
     const {transactions} = useContext(TransactionsContext);
     let wallet = transactions.walletTransactions;
@@ -25,7 +89,7 @@ function ViewCapitalDifference() {
                 }
                 console.log("mining transaction during populating: " + JSON.stringify(walletTransaction, null, 2));
                 let tempMiningTransactions = miningTransactions;
-                tempMiningTransactions.push(walletTransaction)
+                tempMiningTransactions.push(walletTransaction);
                 setMiningTransactions(tempMiningTransactions);
                 //miningTransactions.push(walletTransaction);
             })
@@ -58,13 +122,12 @@ function ViewCapitalDifference() {
         }
 
         let tempMiningTransactions = miningTransactions;
-        tempMiningTransactions.push(walletTransaction)
+        tempMiningTransactions.push(walletTransaction);
         setMiningTransactions(tempMiningTransactions);
         //miningTransactions.push(walletTransaction);
     }
-    
 
-    return ( 
+    return (
         <div>
             <button className="btn btn-primary" onClick={generateReport}>Generate Capital Gain/Loss report</button>
             <button className="btn btn-primary" onClick={addElement}>Add sample Transaction to miningTransactions</button>
@@ -75,3 +138,40 @@ function ViewCapitalDifference() {
 }
 
 export default ViewCapitalDifference;
+
+
+
+
+const [transactions, setTransaction] = useState({value: 1});
+
+    return ( 
+        <div>
+            <h1>Capital Gain/Loss report</h1>
+            <pre>report: {JSON.stringify(transactions, null, 2)}</pre>
+        </div>
+    );
+
+
+
+
+const [transactions, setTransactions] = useState([{date: 11, value: 2}, {date: 12, value: 3}]);
+
+    const addElement = () => {
+        let newTransaction = {date: 13, value: 4};
+        let currentTransactions = [...transactions];
+        console.log(JSON.stringify(currentTransactions, null, 2))
+        currentTransactions.push(newTransaction);
+        console.log(JSON.stringify(currentTransactions, null, 2))
+        setTransactions(currentTransactions);
+        console.log(JSON.stringify(transactions, null, 2))
+        //setTransactions({...transactions});
+    }
+
+    return ( 
+        <div>
+            <h1>Capital Gain/Loss report</h1>
+            <pre>report: {JSON.stringify(transactions, null, 2)}</pre>
+            <button onClick={addElement}>Press to add new element</button>
+        </div>
+    );
+*/
