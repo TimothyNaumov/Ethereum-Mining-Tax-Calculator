8a29f33fab15240080797865d579092b59551fb7
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2022-10-14 16:02:24 -0500

    Fixed Date Representation

--- src/Components/SinglePageViews/GainLossReportView.js
+++ src/Components/SinglePageViews/GainLossReportView.js
@@ -2,17 +2,21 @@ import React from "react";
 import {Container, Table} from 'react-bootstrap'
 import { get8949 } from "../../CapitalGainLossCalculator";
 
-const Transaction = (props) => (
+const Transaction = (props) => {
+    let acqDate = new Date(props.transaction.AcquireDate).toLocaleDateString();
+    let sellDate = new Date(props.transaction.SellDate).toLocaleDateString();
+    return(
     <tr>
-        <td>{props.transaction.AcquireDate}</td>
-        <td>{props.transaction.SellDate}</td>
+        <td>{acqDate}</td>
+        <td>{sellDate}</td>
         <td>{parseFloat(props.transaction.Proceeds).toFixed(2)}</td>
         <td>{parseFloat(props.transaction.CostBasis).toFixed(2)}</td>
         <td>{props.transaction.codes}</td>
         <td>{parseFloat(props.transaction.Adjustment).toFixed(2)}</td>
         <td>{parseFloat(props.transaction.CapitalGainLoss).toFixed(2)}</td>
     </tr>
-);
+    );
+};
 
 const GainLossReportView = (props) => {
     function transactionList(){
