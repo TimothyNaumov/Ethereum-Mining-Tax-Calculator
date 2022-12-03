d35bfb9297772e64063169bf6f1b731a5ffa3855
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2022-12-02 22:35:43 -0600

    Completed Contact form

--- src/pages/HelpPages/Help.Contact.js
+++ src/pages/HelpPages/Help.Contact.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {Container, Row, Col} from 'react-bootstrap';
+import NavigationBar from '../../Components/NavigationBar';
 
 const Contact = () => {
   return (
-    <div>Help.Contact</div>
+    <>
+     <NavigationBar/>
+     <div className='d-flex justify-content-center align-items-center'>
+      <Container style={{width: "650px"}}>
+        <Row className='d-flex' align="center">
+          <h1 className='help-title-text'>Contact Me</h1>
+        </Row>
+        <Row>
+          <Form>
+            <Form.Group className="mb-3" controlId="formFullName">
+              <Form.Label style={{fontSize: "20px"}}>Name</Form.Label>
+              <Form.Control type="name" placeholder="Firstname Lastname" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label style={{fontSize: "20px"}}>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formText">
+              <Form.Label style={{fontSize: "20px"}}>What can I help you with?</Form.Label>
+              <textarea className='form-control' id="formText" rows="3"></textarea>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Row>
+      </Container>
+     </div>
+    </>
+    
   )
 }
 
--- src/pages/HelpPages/Help.ExchangeTransactions.js
+++ src/pages/HelpPages/Help.ExchangeTransactions.js
@@ -1,8 +1,74 @@
 import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
+import NavigationBar from '../../Components/NavigationBar';
 
 const ExchangeTransactionsHelp = () => {
   return (
-    <div>Help.ExchangeTransactions</div>
+    <>
+        <NavigationBar/>
+        <div className='d-flex justify-content-center'>
+            <Container style={{paddingTop:"100px"}}>
+                <Row style={{paddingBottom: "25px"}}>
+                    <div className='d-flex justify-content-start'>
+                        <h1 className='help-title-text'>Exchange Transactions FAQ</h1>
+                    </div>
+                </Row>
+                <Row>
+                <Accordion>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Where do I find my Coinbase exchange transactions?</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>How do I download my Coinbase exchange transactions?</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="2">
+                        <Accordion.Header>Where do I upload my Coinbase exchange transactions?</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="3">
+                        <Accordion.Header>What crypto exchanges can I use?</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </Accordion>
+                </Row>
+            </Container>
+        </div>
+    </>
   )
 }
 
